dd2faa4d23813d38644395cf9e0755cfba0e620d
Padrone56 <pasrone56@gmail.com>
2017-11-10 22:48:28 +0200

    Parse Orcbrew to JSON
    Input string parsed to output JSON string

--- OrcbrewMerger/OrcbrewMerger/Parsing/OrcbrewParser.cs
+++ OrcbrewMerger/OrcbrewMerger/Parsing/OrcbrewParser.cs
@@ -10,10 +10,65 @@ namespace OrcbrewMerger.Parsing
     public static class OrcbrewParser
     {
 
-        public static void ParseOrcbrewFile(string input)
+        public static string ParseOrcbrewFile(string input)
         {
             
-            
+            sFileString = new System.Text.StringBuilder(reader.ReadToEnd());
+                            
+            bool removed = false;
+            bool inString = false;
+            for (int i = 0; i < sFileString.Length; i++)
+            {
+                
+                char cChar = sFileString[i];
+                if (cChar.Equals('\\') && sFileString[i + 1] == 'n')
+                {
+                    sFileString.Remove(i, 2);
+                }
+                if (cChar.Equals('"'))
+                {
+                    inString = !inString;
+                }
+                if (cChar.Equals(':') && sFileString[i+1] != ' ' && !inString)
+                {
+                    sFileString[i] = '"';
+                    removed = true;
+                    continue;
+                }
+                if (removed && cChar.Equals('}'))
+                {
+                    sFileString.Insert(i, "\"");
+                    i = i + 1;
+                    removed = false;
+                    continue;
+                }
+                if (removed && cChar.Equals(','))
+                {
+                    sFileString.Insert(i, "\"");
+                    i = i + 1;
+                    removed = false;
+                    continue;
+                }
+                if (removed && Char.IsWhiteSpace(cChar))
+                {
+                    sFileString.Insert(i, "\": ");
+                    i = i + 2;
+                    removed = false;
+                    continue;
+                }
+                if (cChar.Equals('}'))
+                {
+                    if (i < sFileString.Length - 2 && sFileString[i+1] == ' ' && sFileString[i+2] == '{')
+                    {
+                        sFileString.Insert(i+1, ',');
+                        i = i + 2;
+                    }
+                    continue;
+                }
+
+                }
+
+            return sFileString.toString();
             
         }
 
